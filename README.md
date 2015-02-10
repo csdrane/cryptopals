17fbf3fecb9ea4072f50d23a6f65e8221b6c280d
Chris Drane <csdrane@gmail.com>
2015-02-10 16:08:54 -0500

    Added license and readme.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cryptopals
+
+My solutions for [Cryptopals](http://cryptopals.com) problem sets 1 and 2, written in Clojure.
+
+## License
+
+Eventless is licensed under the MIT License. See LICENSE for more information.
--- src/cryptopals/set2.clj
+++ src/cryptopals/set2.clj
@@ -103,77 +103,163 @@
                 (concat user-input prob-11-secret-base64)
                 16))))
 
-(map identity (ecb-oracle []))
-
-(defn create-table [encryption-function padding-byte & [found-bytes]]
-  {:docstring "Returns hash-map mapping byte-array one byte short of block size to resulting output."}
-  (let [found-bytes (if (nil? (first found-bytes)) nil found-bytes)
-        padding-len (- 15 (count found-bytes))
-        padding-bytes (repeat padding-len padding-byte)
-        initial-bytes (concat padding-bytes (if-not (empty? found-bytes) found-bytes))]
-    (letfn [(to-vec [byte-array]
-              (into [] (map identity byte-array)))]
-      (into {} (for [last-byte (range 0 255)]
-                 (let [bytes (byte-array (concat initial-bytes (list last-byte)))]
-                   {(to-vec (take 16 (map identity (encryption-function bytes)))) (to-vec bytes)}))))))
+;; (defn create-table [encryption-function padding-byte & [found-bytes]]
+;;   {:docstring "Returns hash-map mapping byte-array one byte short of block size to resulting output."}
+;;   (let [found-bytes (if (nil? (first found-bytes)) nil found-bytes)
+;;         padding-len (- 15 (count found-bytes))
+;;         padding-bytes (repeat padding-len padding-byte)
+;;         initial-bytes (concat padding-bytes (if-not (empty? found-bytes) found-bytes))]
+;;     (letfn [(to-vec [byte-array]
+;;               (into [] (map identity byte-array)))]
+;;       (into {} (for [last-byte (range 0 255)]
+;;                  (let [bytes (byte-array (concat initial-bytes (list last-byte)))]
+;;                    {(to-vec (take 16 (map identity (encryption-function bytes)))) (to-vec bytes)}))))))
 
 ; add another assertion for byte-pos + found-bytes?
-(defn get-byte [byte-pos padding-byte & [found-bytes]]
-  {:pre [(and (< 0 byte-pos 17)
-              (integer? padding-byte))]}
-  (let [padding-len (- 16 byte-pos)
-        padding (repeat padding-len padding-byte)
-        table (create-table ecb-oracle padding-byte (if-not (empty? found-bytes) found-bytes))
-        bytes-for-oracle (byte-array padding)
-        oracle-output (into [] (take 16 (ecb-oracle bytes-for-oracle)))]
-    (get table oracle-output)))
+;; (defn get-byte [byte-pos padding-byte & [found-bytes]]
+;;   {:pre [(and (< 0 byte-pos 17)
+;;               (integer? padding-byte))]}
+;;   (let [padding-len (- 16 byte-pos)
+;;         padding (repeat padding-len padding-byte)
+;;         table (create-table ecb-oracle padding-byte (if-not (empty? found-bytes) found-bytes))
+;;         bytes-for-oracle (byte-array padding)
+;;         oracle-output (into [] (take 16 (ecb-oracle bytes-for-oracle)))]
+;;     (get table oracle-output)))
 
-(defn get-first-plaintext-block []
-  (map identity
-       (loop [pos 1
-              found-bytes (vector-of :byte)]
-         (if (= pos 17)
-           found-bytes
-           (let [new-byte (byte (last (get-byte pos 65 found-bytes)))]
-             (recur (inc pos) (conj found-bytes new-byte)))))))
+;; (defn get-first-plaintext-block []
+;;   (map identity
+;;        (loop [pos 1
+;;               found-bytes (vector-of :byte)]
+;;          (if (= pos 17)
+;;            found-bytes
+;;            (let [new-byte (byte (last (get-byte pos 65 found-bytes)))]
+;;              (recur (inc pos) (conj found-bytes new-byte)))))))
 
-(defn find-byte [encryption-function attack-bytes]
-  (let [attack-ciphertext (into [] (take 16 (encryption-function attack-bytes)))]
-    (last (get (letfn [(to-vec [byte-array]
-                         (into [] (map identity byte-array)))]
-                 (into {} (for [last-byte (range 0 255)]
-                            (let [bytes (byte-array (concat attack-bytes (list last-byte)))]
-                              {(to-vec (take 16 (map identity (encryption-function bytes))))
-                               (to-vec bytes)}))))
-               attack-ciphertext))))
+;; (defn find-byte [encryption-function attack-bytes]
+;;   (let [attack-ciphertext (into [] (take 16 (encryption-function attack-bytes)))]
+;;     (last (get (letfn [(to-vec [byte-array]
+;;                          (into [] (map identity byte-array)))]
+;;                  (into {} (for [last-byte (range 0 255)]
+;;                             (let [bytes (byte-array (concat attack-bytes (list last-byte)))]
+;;                               {(to-vec (take 16 (map identity (encryption-function bytes))))
+;;                                (to-vec bytes)}))))
+;;                attack-ciphertext))))
 
-(find-byte ecb-oracle first-plaintext-block)
-(find-byte ecb-oracle (byte-array (drop 1 (concat first-plaintext-block '(82)))))
-(take 32 (map identity (ecb-oracle [])))
-(take 16 (ecb-oracle first-plaintext-block))
+;; (find-byte ecb-oracle first-plaintext-block)
+;; (find-byte ecb-oracle (byte-array (drop 1 (concat first-plaintext-block '(82)))))
+;; (take 32 (map identity (ecb-oracle [])))
+;; (take 16 (ecb-oracle first-plaintext-block))
 
-(find-byte ecb-oracle (drop 1 first-plaintext-block))
+;; (find-byte ecb-oracle (drop 1 first-plaintext-block))
 
-(defn decrypt-remainder [first-block]
-  (loop [ciphertext (ecb-oracle [])
-         attack-bytes first-block
-         coll (into [] first-block)]
-    (if (empty? ciphertext)
-      coll
-      (do (println attack-bytes coll)
-          (let [new-ciphertext (drop 1 ciphertext)
-                new-attack-bytes (concat (drop 1 attack-bytes)
-                                         (list (first ciphertext)))
-                new-coll (find-byte ecb-oracle attack-bytes)]
-            (recur new-ciphertext new-attack-bytes new-coll))))))
+;; (defn decrypt-remainder [first-block]
+;;   (loop [ciphertext (ecb-oracle [])
+;;          attack-bytes first-block
+;;          coll (into [] first-block)]
+;;     (if (empty? ciphertext)
+;;       coll
+;;       (do (println attack-bytes coll)
+;;           (let [new-ciphertext (drop 1 ciphertext)
+;;                 new-attack-bytes (concat (drop 1 attack-bytes)
+;;                                          (list (first ciphertext)))
+;;                 new-coll (find-byte ecb-oracle attack-bytes)]
+;;             (recur new-ciphertext new-attack-bytes new-coll))))))
 
-(decrypt-remainder first-plaintext-block)
+;; (decrypt-remainder first-plaintext-block)
 
-(def first-plaintext-block (get-first-plaintext-block))
+; (def first-plaintext-block (get-first-plaintext-block))
 
-(defn get-first-byte []
-  (last (get-byte 1 65)))
+;; (defn get-first-byte []
+;;   (last (get-byte 1 65)))
 
-(take 16 (map identity (ecb-oracle (byte-array (drop 1 (concat first-plaintext-block '(82)))))))
+; (take 16 (map identity (ecb-oracle (byte-array (drop 1 (concat first-plaintext-block '(82)))))))
 
 ;; TODO rewrite to start with enough padding to hold entire decryption
+
+;; (defn get-first-plaintext-block2 []
+;;   (let [block-size 16
+;;         repeats 10]
+;;     (loop [padding (dec (* repeats block-size))
+;;            block-index (inc (* (dec repeats)
+;;                                block-size)) ; first byte of block 10
+;;            found-bytes []]
+;;       (let [attack-bites ()]
+;;         (if (= padding 0)
+;;           found-bytes
+;;           (let [new-byte (byte (last (find-byte ecb-oracle attack-bytes)))]
+;;             (recur (dec padding) (dec block-index) (conj found-bytes new-byte))))))))
+
+;; (defn find-byte [padding found-bytes]
+;;   (for [last-byte (range 0 255)]
+;;     (let [last-known-15-bytes (let [found-bytes [82 19]
+;;                                     padding (repeat 100 65)
+;;                                     init (take-last 15 found-bytes)]
+;;                                 (concat (take (- 15 (count init)) padding) init))
+;;           oracle-input (concat last-known-15-bytes (list last-byte))
+;;           oracle-output (take 16 (ecb-oracle oracle-input))
+;;           ]
+;;       {oracle-output oracle-input}
+;;       )))
+
+;; ((65 65 65 65 65 65 65 65 65 65 65 65 65 82 19 0))
+;; maps to 
+;; (10 -4 117 16 -78 86 -15 -85 -80 34 -56 -36 77 -12 117 -40))
+
+;; (find-byte (repeat 160 65) [])
+
+;; (fn []
+;;   (letfn []
+;;     (loop [padding (repeat (dec (* repeats block-size)) 65)
+;;            found-bytes []]
+;;       (if (= 0 (count padding))
+;;         found-bytes
+;;         (let [block-size 16
+;;               repeats 10
+;;               new-found-bytes (find-byte padding found-bytes)]
+;;           (recur (drop-last padding)
+;;                  new-found-bytes))))))
+
+;;          BLOCK 1                     BLOCK 2
+
+;; 65 65 65 65 65 65 65 65 65 65 65   82 82 82 82 82 82 82 82 82
+
+
+
+;;;;;;;;
+
+(defn get-test [pos]
+  "Returns 16 byte block ending at position pos."
+  (let [padding (- 16 (mod 16 16))
+        ciphertext (map identity (ecb-oracle (drop 1 (repeat padding 65))))
+        ciphertext-trimmed-front (drop (dec pos) ciphertext)
+        ciphertext-fully-trimmed (drop-last (- (count ciphertext-trimmed-front)
+                                               16) ciphertext-trimmed-front)]
+    ciphertext-fully-trimmed))
+
+(defn build-dict [known-15]
+         "Returns map of 16 byte blocks to ciphertext outputs"
+         (into {} (for [last-byte (range 0 255)]
+                    (let [input (concat known-15 (list last-byte))
+                          output (map identity (take 16 (ecb-oracle input)))]
+                      {output input}))))
+
+(let [ciphertext-len (count (ecb-oracle []))]
+  (loop [known-bytes []
+         bytes-for-build-dict (into [] (repeat 15 65))
+         pos 1]
+    (do (println known-bytes bytes-for-build-dict pos)
+        (if (> pos ciphertext-len)
+          known-bytes
+          (let [dict (build-dict bytes-for-build-dict)
+                test-pos (get-test pos)
+                byte (last (get dict test-pos))]
+            (recur (conj known-bytes byte)
+                   (conj (subvec bytes-for-build-dict 1) byte)
+                   (inc pos)))))))
+
+(get (build-dict (count (concat (repeat 14 65) (list 82))))
+     (get-test 2))
+
+(build-dict (conj (into [] (repeat 15 65)) 82))
+(build-dict (repeat 16 65))
+(get-test 2)
